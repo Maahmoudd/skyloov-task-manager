326c0ade6b547b17b968cbf00e63d4d313e60e99
Mahmoud El Sabahy <mahmoud.elsabahy@brightcreations.com>
2024-10-10 00:43:10 +0300

    Seeded the database with admin user with some tasks

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\TaskStatusEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(2),
+            'description' => $this->faker->sentence(10),
+            'status' => TaskStatusEnum::PENDING->value,
+            'due_date' => $this->faker->dateTimeBetween(now(), now()->addDays(3)),
+        ];
+    }
+}
